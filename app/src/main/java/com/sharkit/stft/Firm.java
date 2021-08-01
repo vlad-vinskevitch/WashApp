38c88726ce5545c16ee1339cf54d30c54c292e7e
chaosmaster <vvinskevitch@gmail.com>
2021-08-01 15:50:55 +0300

    NEW

---
+++ app/src/main/java/com/sharkit/stft/Firm.java
@@ -0,0 +1,14 @@
+package com.sharkit.stft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Firm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firm);
+    }
+}
